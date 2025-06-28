d23534805bbe6c633d42dac45d98594560123101
Harald Wartig <hwartig@gmail.com>
2025-06-28 13:41:41 +0200

    Add nvim-lspconfig for denols and vtsls.

--- .config/nvim/lua/plugins/core.lua
+++ .config/nvim/lua/plugins/core.lua
@@ -27,4 +27,23 @@ return {
     dependencies = { { "echasnovski/mini.icons", opts = {} } },
     -- dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if prefer nvim-web-devicons
   },
+  { "mason.nvim" },
+  { "mason-lspconfig.nvim" },
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      servers = {
+        vtsls = {
+          root_dir = require("lspconfig").util.root_pattern("package.json", "tsconfig.json"),
+          single_file_support = false,
+          settings = {},
+        },
+        denols = {
+          root_dir = require("lspconfig").util.root_pattern("deno.json", "deno.jsonc"),
+          single_file_support = false,
+          settings = {},
+        },
+      },
+    },
+  },
 }
